63b1e3eba003eab9eb29e9b25b649d38bc804316
phatchara009 <76895830+phatchara009@users.noreply.github.com>
2021-01-28 17:06:03 +0700

    Add files via upload

---
+++ 1-1_BasicOperations.R
@@ -0,0 +1,60 @@
+setwd("C:/Users/Admin/Desktop/RStudioZ/introductiontoR")
+getwd()
+Sys.setlocale("LC_CTYPE", "Thai")
+options(encoding="UTF-8")
+##########################################################
+
+#Comment
+1+1
+
+# My Formula
+2*100-5
+
+#Print function
+print("Hello world")
+
+# Create Object #
+# Object-Oriented Programming
+
+# Assign ( <- )
+x <- 1
+y <- 2
+x+y
+
+# Remove object
+rm(x)
+rm(y)
+
+# data types #
+# 1. numeric ( type )
+# 2. logical ( TRUE or FALSE )
+# 3. character
+
+#numeric
+class(x) # check data type class(c)
+x <- 2
+class(x)
+
+# Check Logical  
+# == equal sign 
+# != Not equal sign
+# Ex: 1+1 == 2 / 1+1 != 2
+1+1 != 2
+1+1 == 2
+pi
+class(TRUE) 
+class(FALSE)
+
+#Character
+my_name <- "Toy"
+my_name # or print(my_name)
+class(my_name)
+
+your_name <- "Student"
+your_name # or print(your_name)
+class(your_name)
+
+
+#¡¡
+
+
